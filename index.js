8952cf65b59d4d28fb910a661b6b50ba8e7d9267
lenselinko31 <lenselink.olivia@gmail.com>
2022-11-21 19:34:00 -0600

    Added forecast

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ function tempUpdate(city) {
   axios.get(cityUrl).then(currentTemp);
 }
 
+import "./styles.css";
+
+navigator.geolocation.getCurrentPosition(pageLoad);
+
+//Format form and heading
+function cityName(intro) {
+  intro.preventDefault();
+  let city = document.querySelector("#city").value;
+  let header = document.querySelector("#heading");
+  header.innerHTML = city;
+  tempUpdate(city);
+  tempC.classList.remove("active");
+  temp.classList.add("active");
+}
+
+//Change city name to match entered city
+function tempUpdate(city) {
+  let citySearched = document.querySelector("#city").value;
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&units=imperial&appid=${apiKey}`;
+  axios.get(cityUrl).then(currentTemp);
+}
+
 let form = document.querySelector("form");
 form.addEventListener("submit", cityName);
 
@@ -69,16 +92,65 @@ function location(intro) {
       "&units=imperial&appid=" +
       apiKey;
     axios.get(url).then(currentTemp);
+    tempC.classList.remove("active");
+    temp.classList.add("active");
   }
   navigator.geolocation.getCurrentPosition(findLocation);
 }
 
-//Updates weather for city
 document.querySelector("#current").addEventListener("click", location);
 document.addEventListener("DOMContentLoaded", function () {
   location();
 });
 
+function forecastWeekDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+//display forcasted weather
+function forecastCards(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index >= 1 && index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class = ""row>
+                <div class="forecastDate">${forecastWeekDay(
+                  forecastDay.dt
+                )}</div>
+                <img
+                src="https://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png" class="forecastDate" alt=""
+                width="70"/>
+                <div class="high">${Math.round(
+                  forecastDay.temp.max
+                )}° <span class = "low">${Math.round(
+          forecastDay.temp.min
+        )}°</span></div>
+            </div>
+            </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function findForecast(coordinates) {
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let forecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(forecastUrl).then(forecastCards);
+}
+
+//Updates weather for city
 function currentTemp(response) {
   let name = response.data.name;
   let country = response.data.sys.country;
@@ -99,10 +171,13 @@ function currentTemp(response) {
   let icon = document.querySelector("#icon");
   icon.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  findForecast(response.data.coord);
 }
 
+//Change temp to Fahfrenheit
 function ToF(event) {
   event.preventDefault();
   let currentTemp = document.querySelector("#temperature");
@@ -111,6 +186,7 @@ function ToF(event) {
   currentTemp.innerHTML = fTemp;
 }
 
+//Change temp to Celsius
 function ToC(event) {
   event.preventDefault();
   let currentTemp = document.querySelector("#temperature");
@@ -128,6 +204,7 @@ temp.addEventListener("click", ToF);
 let tempC = document.querySelector("#tempChangeC");
 tempC.addEventListener("click", ToC);
 
+//Display weather for current location on load
 function pageLoad() {
   function findLocation(position) {
     let lat = position.coords.latitude;
@@ -141,6 +218,8 @@ function pageLoad() {
       "&units=imperial&appid=" +
       apiKey;
     axios.get(url).then(currentTemp);
+    temp.classList.add("active");
+    tempC.classList.remove("active");
   }
   navigator.geolocation.getCurrentPosition(findLocation);
 }
